94e68a8fcc7fba6dd684230db0eccc908c4c52d8
vetadigitalstore <vetadigitalstore@gmail.com>
2025-01-20 12:55:49 +0530

    Delete main directory

--- main/Dashboard/category/script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementById('theme-toggle');
-const body = document.body;
-
-toggleButton.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
-});
