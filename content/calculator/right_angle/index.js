ae1fe5c185ac9225d422b2ee64114d97d9aabe26
vetadigitalstore <vetadigitalstore@gmail.com>
2025-01-24 15:50:12 +0530

    Delete content/calculator/right_angle/index.js

--- content/calculator/right_angle/index.js
+++
@@ -1,7 +0,0 @@
-// Mode Toggle Script
-const modeToggle = document.getElementById('modeToggle');
-const body = document.body;
-
-modeToggle.addEventListener('click', () => {
-  body.setAttribute('data-theme', body.getAttribute('data-theme') === 'dark' ? '' : 'dark');
-});
