0dc17153f0f06374f5237e01c9b5a8684973ac99
vetadigitalstore <vetadigitalstore@gmail.com>
2025-01-24 13:03:40 +0530

    Delete home directory

--- home/body/body.js
+++
@@ -1,8 +0,0 @@
-function toggleTheme() {
-    const currentTheme = document.documentElement.getAttribute("data-theme");
-    if (currentTheme === "dark") {
-        document.documentElement.setAttribute("data-theme", "light");
-    } else {
-        document.documentElement.setAttribute("data-theme", "dark");
-    }
-}
